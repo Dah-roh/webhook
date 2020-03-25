3f616b7a0fc7a27511b07cdd16e5c8297e42541e
dizsart <darogadibia@gmail.com>
2020-03-25 15:12:32 +0100

    fix for authorization bearer

--- samplerest/src/main/java/com/example/samplerest/controller.java
+++ samplerest/src/main/java/com/example/samplerest/controller.java
@@ -18,68 +18,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import javax.print.attribute.URISyntax;
-import java.io.DataInput;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.sql.Date;
-import java.util.List;
 
 @RestController
 @RequestMapping("/")
 public class controller {
-
-//    @Autowired
-//    private AuthorizationRepository authorizationRepository;
-//    @Autowired
-//    private CustomerRepository customerRepository;
-//    @Autowired
-//    private DataRepository dataRepository;
-//    @Autowired
-//    private HistoryRepository historyRepository;
-//    @Autowired
-//    private LogRepository logRepository;
-//    @Autowired
-//    private StatusRepository statusRepository;
-
-
-//    @RequestMapping("/verification")
-//    public String sendPost() throws URISyntaxException, IOException {
-//
-//            RestTemplate restTemplate = new RestTemplate();
-//            final String baseUrl = "http://localhost:8890/verification/";
-//            URI uri = new URI(baseUrl);
-//            String date = "2020-10-01";
-//            Date date1 = Date.valueOf(date);
-//            String reference = "DG4uishudoq90LD";
-//            Customer customer = new Customer(84312L, "CUS_hdhye17yj8qd2tx", "BoJack", "Horseman", "bojack@horseman.com");
-//            Authorization authorization = new Authorization("AUTH_8dfhjjdt", "visa", "1381", "08", "2018", "412345", "TEST BANK", "card", true, "NG");
-//            History history = new History("input", "Filled these fields: card number, card expiry, card cvv", 7);
-//            historyRepository.save(history);
-//            Log log = new Log( 9, 1, null, 0, true, false, null, null, history);
-//        customerRepository.save(customer);
-//        authorizationRepository.save(authorization);
-//        logRepository.save(log);
-//            Data data = new Data(27000l, "NGN", date1, "success", reference, "test", 0, "Successful", null, "card", "41.1.25.1", log, null, authorization, customer, "PLN_0as2m9n02c10kp6");
-//            Status status = new Status(false, "Verification successful", data);
-//            dataRepository.save(data);
-//            statusRepository.save(status);
-//            HttpHeaders headers = new HttpHeaders();
-//            headers.set("X-COM-PERSIST", "true");
-//            headers.set("X-COM-LOCATION", "NIGERIA");
-//            ObjectMapper mapper = new ObjectMapper();
-//        mapper.configure(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT, true);
-//            String stat = mapper.writeValueAsString(status);
-//            HttpEntity<?> request = new HttpEntity<>(stat, headers);
-//        System.out.println(status);
-//            ResponseEntity<String> send = restTemplate.postForEntity(uri, request, String.class);
-//
-//        if(send!=null){
-//            return "Success";
-//        }
-//        return "failed";
-//    }
 
     @RequestMapping("/verify")
     public String sendPost() throws URISyntaxException, IOException {
@@ -161,6 +106,7 @@ public class controller {
         HttpHeaders headers = new HttpHeaders();
         headers.set("X-COM-PERSIST", "true");
         headers.set("X-COM-LOCATION", "Nigeria");
+        headers.set("Authorization", "Bearer sk_test_81faa90c2aad40bae29d9e9319ff6ebfd13a459b");
         headers.setContentType(MediaType.APPLICATION_JSON);
         HttpEntity<String> request = new HttpEntity<>(payStackJson, headers);
         ResponseEntity<String> send = restTemplate.postForEntity(uri, request, String.class);
